3136ec4b67f28b3d9fb4078fb0ad5d22fa8caf1a
Adel Rustum <adel_restom@yahoo.com>
2018-11-29 10:47:15 -0700

    Fixes No. 1

--- cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -3,7 +3,10 @@ package com.template
 import net.corda.core.contracts.*
 import net.corda.core.identity.AbstractParty
 import net.corda.core.transactions.LedgerTransaction
+import net.corda.core.identity.Party
+import net.corda.core.identity.AnonymousParty
 import java.security.PublicKey
+import java.util.*
 
 // ************
 // * Contract *
@@ -26,13 +29,10 @@ class MyCashContract : Contract {
                 requireThat {
                     // Generic constraints around the IOU transaction.
                     "No inputs should be consumed when issuing new cash." using (tx.inputs.isEmpty())
-                    "Only one output state should be created." using (tx.outputs.size == 1)
-                    val out = tx.outputsOfType<MyCash>().single()
-                    "The issuer and the owner cannot be the same entity." using (out.issuer != out.owner)
-                    "Only the issuer can create new cash." using (command.signers.contains(out.issuer))
-
-                    // MyCash-specific constraints.
-                    "The cash value must be non-negative." using (out.value > 0)
+                    "At least one output state should be created." using (tx.outputs.isNotEmpty())
+                    val outputs = tx.outputsOfType<MyCash>()
+                    "The issuer and the owner cannot be the same entity." using outputs.filter { it.issuer == it.owner }.isNotEmpty()
+                    "Only the issuer can create new cash." using command.signers.containsAll(outputs.map { it.issuer.owningKey })
                 }
             }
 
@@ -40,13 +40,9 @@ class MyCashContract : Contract {
                 requireThat {
                     // Generic constraints around the IOU transaction.
                     "One or more inputs should be consumed when moving cash." using (tx.inputs.isNotEmpty())
-                    "Only one output state should be created." using (tx.outputs.size == 1)
-                    val out = tx.outputsOfType<MyCash>().single()
-                    "The new owner and the old owner cannot be the same entity." using (out.oldOwner != out.owner)
-                    "Old owner must sign the move." using (command.signers.contains(out.oldOwner))
-
-                    // MyCash-specific constraints.
-                    "The cash value must be non-negative." using (out.value > 0)
+                    "At least one output state should be created." using (tx.outputs.isNotEmpty())
+                    val outputs = tx.outputsOfType<MyCash>()
+                    "Old owner must sign the move." using command.signers.containsAll(outputs.map { it.oldOwner.owningKey })
                 }
             }
 
@@ -55,8 +51,8 @@ class MyCashContract : Contract {
                     // Generic constraints around the IOU transaction.
                     "One or more inputs should be consumed when destroying cash." using (tx.inputs.isNotEmpty())
                     "There should be no outputs." using (tx.outputs.isEmpty())
-                    val input = tx.inputsOfType<MyCash>().single()
-                    "Both issuer and owner must sign the exit command." using command.signers.containsAll(input.exitKeys.map { it.owningKey })
+                    val inputs = tx.inputsOfType<MyCash>()
+                    "Issuer and Owners must sign the exit command." using command.signers.containsAll(inputs.map { it.exitKeys[0]; it.exitKeys[1] })
                 }
             }
 
@@ -78,24 +74,32 @@ class MyCashContract : Contract {
 // *********
 // * State *
 // *********
-data class MyCash(val issuer: party,
-                  override var owner: party,
-                  override var amount: Long) :
-        FungibleAsset<Amount<Issued<Currency>>> {
+data class MyCash(val issuer: AbstractParty,
+                  override val owner: AbstractParty,
+                  override var amount: Amount<Issued<Currency>>) :
+        FungibleAsset<Currency>{
 
-    var oldOwner: party = AnonymousParty(NullKeys.NullPublicKey)
+    var oldOwner: AbstractParty = owner
 
     // Issuer and owner should be aware of this state
-    override val participants = listOf(issuer, owner)
+    override var participants = listOf(issuer, owner)
 
-    // Issuer and owner must sign the exit command to destroy cash amount (i.e. move it off-ledger)
-    override val exitKeys = listOf(issuer, owner)
+    // Issuer and owner must sign exit command to destroy the cash amount (i.e. move it off-ledger)
+    override var exitKeys = listOf(issuer.owningKey, owner.owningKey)
 
     override fun withNewOwner(newOwner: AbstractParty): CommandAndState {
-        copy(oldOwner = owner, owner = newOwner)
+        val updatedCash = copy(owner = newOwner)
+        updatedCash.oldOwner = owner
+        updatedCash.participants = listOf(issuer, owner)
+        updatedCash.exitKeys = listOf(issuer.owningKey, owner.owningKey)
+        return CommandAndState(MyCashContract.Commands.Move(), updatedCash)
     }
 
-    override fun withNewOwnerAndAmount(newAmount: Amount<Issued<Amount<Issued<>>>>, newOwner: AbstractParty): FungibleAsset<Amount<Issued<>>> {
-        copy(oldOwner = owner, owner = newOwner, amount = newAmount)
+    override fun withNewOwnerAndAmount(newAmount: Amount<Issued<Currency>>, newOwner: AbstractParty): FungibleAsset<Currency> {
+        val updatedCash = copy(owner = newOwner, amount = newAmount)
+        updatedCash.oldOwner = owner
+        updatedCash.participants = listOf(issuer, owner)
+        updatedCash.exitKeys = listOf(issuer.owningKey, owner.owningKey)
+        return updatedCash
     }
 }
